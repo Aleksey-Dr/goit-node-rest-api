cd787e2fc24b90fbc6ff9a3cc6e04dbcf7353338
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2024-05-28 21:09:23 +0300

    add REST API for list contacts

---
+++ middlewares/isEmptyBody.js
@@ -0,0 +1,9 @@
+import HttpError from "../helpers/HttpError.js";
+
+export default function isEmptyBody (req, _, next) {
+  const { length } = Object.keys(req.body);
+  if (!length) {
+    return next(HttpError(400, "Body must have at least one field"));
+  }
+  next();
+};
